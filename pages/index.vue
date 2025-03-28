43f983e7fe7fb2af75e8c31ae40cbc8d8328eade
Alex Gaillard <alex@directus.io>
2025-03-28 10:58:54 -0400

    Add directusUrl

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,30 @@
 <template>
-	<h1 :data-directus="setAttr({collection: 'global', item: '1', field: 'title'})">{{global.title}}</h1>
-	<p :data-directus="setAttr({collection: 'global', item: '1', field: 'description'})">{{global.description}}</p>
+  <h1
+    :data-directus="
+      setAttr({ collection: 'global', item: '1', field: 'title' })
+    "
+  >
+    {{ global.title }}
+  </h1>
+  <p
+    :data-directus="
+      setAttr({ collection: 'global', item: '1', field: 'description' })
+    "
+  >
+    {{ global.description }}
+  </p>
 </template>
 
 <script setup>
-import {apply, setAttr} from '@directus/visual-editing'
-const { $directus, $readItems } = useNuxtApp()
+import { apply, setAttr } from "@directus/visual-editing";
+const { $directus, $readItems } = useNuxtApp();
+const { directusUrl } = useRuntimeConfig().public;
 
-const { data: global } = await useAsyncData('global', () => {
-  return $directus.request($readItems('global'))
-})
+const { data: global } = await useAsyncData("global", () => {
+  return $directus.request($readItems("global"));
+});
 
 onMounted(() => {
-  apply({directusUrl: 'http://localhost:8080'})
-})
+  apply({ directusUrl });
+});
 </script>
