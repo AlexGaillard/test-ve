1ebb596d8989acb7a73d19078f3aaa842e30afee
Alex Gaillard <alex@directus.io>
2025-03-28 10:40:10 -0400

    Initial commit and working

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1 :data-directus="setAttr({collection: 'global', item: '1', field: 'title'})">{{global.title}}</h1>
+	<p :data-directus="setAttr({collection: 'global', item: '1', field: 'description'})">{{global.description}}</p>
+</template>
+
+<script setup>
+import {apply, setAttr} from '@directus/visual-editing'
+const { $directus, $readItems } = useNuxtApp()
+
+const { data: global } = await useAsyncData('global', () => {
+  return $directus.request($readItems('global'))
+})
+
+onMounted(() => {
+  apply({directusUrl: 'http://localhost:8080'})
+})
+</script>
